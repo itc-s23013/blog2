0ac128e2b9855a43422cd23ae68aa716a6a4f512
苦しみ悶える <s23013@std.it-college.ac.jp>
2024-02-01 05:46:39 +0000

    8-3

---
+++ next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    domains: ['images.microms-assets.io']
+  }
+}
+
+module.exports = nextConfig
