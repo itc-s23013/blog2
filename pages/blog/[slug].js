11ae1d677979636703bbdb5b09065f7490efa36c
苦しみ悶える <s23013@std.it-college.ac.jp>
2024-02-20 01:58:41 +0000

    戻りのリンクが治ってほしい 9-4

--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -11,9 +11,11 @@ import {
 } from 'components/two-column'
 import ConvertBody from 'components/convert-body'
 import PostCategories from 'components/post-categories'
+import Pagination from 'components/pagination'
 import Image from 'next/image'
 import { getPlaiceholder } from 'plaiceholder'
 import { eyecatchLocal } from 'lib/constants'
+import { prevNextPost } from 'lib/prev-next-post'
 
 const Post = ({
   title,
@@ -21,7 +23,9 @@ const Post = ({
   content,
   eyecatch,
   categories,
-  description
+  description,
+  prevPost,
+  nextPost
 }) => {
   return (
     <Container>
@@ -58,6 +62,19 @@ const Post = ({
             <PostCategories categories={categories} />
           </TwoColumnSidebar>
         </TwoColumn>
+
+        <Pagination
+          prevText={prevPost.title}
+          prevURL={`/blog/${prevPost.slug}`}
+          nextText={nextPost.title}
+          nextUrl={`/blog/${nextPost.slug}`}
+        />
+        <div>
+          {prevPost.title} {prevPost.slug}
+        </div>
+        <div>
+          {nextPost.title} {nextPost.slug}
+        </div>
       </article>
     </Container>
   )
@@ -69,7 +86,7 @@ const getStaticPaths = async () => {
 
   return {
     paths: allSlugs.map(({ slug }) => `/blog/${slug}`),
-    fallback: false
+    fallback: 'blocking'
   }
 }
 
@@ -79,22 +96,30 @@ const getStaticProps = async context => {
   const slug = context.params.slug
 
   const post = await getPostBySlug(slug)
+  if (!post) {
+    return { notFound: true }
+  } else {
+    const description = extractText(post.content)
 
-  const description = extractText(post.content)
+    const eyecatch = post.eyecatch ?? eyecatchLocal
 
-  const eyecatch = post.eyecatch ?? eyecatchLocal
+    const { base64 } = await getPlaiceholder(eyecatch.url)
+    eyecatch.blurDataURL = base64
 
-  const { base64 } = await getPlaiceholder(eyecatch.url)
-  eyecatch.blurDataURL = base64
+    const allSlugs = await getAllSlugs()
+    const [prevPost, nextPost] = prevNextPost(allSlugs, slug)
 
-  return {
-    props: {
-      title: post.title,
-      publish: post.publishDate,
-      content: post.content,
-      eyecatch,
-      categories: post.categories,
-      description
+    return {
+      props: {
+        title: post.title,
+        publish: post.publishDate,
+        content: post.content,
+        eyecatch: eyecatch,
+        categories: post.categories,
+        description: description,
+        prevPost: prevPost,
+        nextPost: nextPost
+      }
     }
   }
 }
